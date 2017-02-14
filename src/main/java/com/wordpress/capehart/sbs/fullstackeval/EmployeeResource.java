15be9efb7e2ed7fb82c9d346309581d60f23986e
Joshua Capehart <joshuacapehart@gmail.com>
2017-02-14 17:02:11 -0500

    Add 404 on not found when using id in REST requests.

--- src/main/java/com/wordpress/capehart/sbs/fullstackeval/EmployeeResource.java
+++ src/main/java/com/wordpress/capehart/sbs/fullstackeval/EmployeeResource.java
@@ -1,7 +1,6 @@
 package com.wordpress.capehart.sbs.fullstackeval;
 
 import java.net.URI;
-import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -9,6 +8,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,11 +17,12 @@ import org.bson.json.JsonMode;
 import org.bson.json.JsonWriterSettings;
 import org.bson.types.ObjectId;
 
-import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
+
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Projections.*;
 
@@ -76,14 +77,17 @@ public class EmployeeResource {
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getIt(@PathParam("id") String id) {
+    public String getIEmployee(@PathParam("id") String id) {
     	// TODO: Make this get the DB host and port from a config file
     	try(MongoClient mongoClient = new MongoClient()) {
 	    	MongoDatabase db = mongoClient.getDatabase("sbs");
 	    	MongoCollection<Document> employeesDBCollection = db.getCollection("employees");
 	    	
-	    	return employeesDBCollection.find(eq("_id", new ObjectId(id))).first().
-	    			toJson(new JsonWriterSettings(JsonMode.STRICT));
+	    	Document employee = employeesDBCollection.find(eq("_id", new ObjectId(id))).first();
+	    	if(employee == null) {
+	    		throw new WebApplicationException(Response.Status.NOT_FOUND);
+	    	}
+	    	return employee.toJson(new JsonWriterSettings(JsonMode.STRICT));
     	}
     }
     
@@ -113,7 +117,14 @@ public class EmployeeResource {
 	    	MongoCollection<Document> employeesDBCollection = db.getCollection("employees");
 
 	    	Document newEmployeeDoc = Document.parse(newEmployee);
-	    	employeesDBCollection.replaceOne(eq("_id", new ObjectId(id)), newEmployeeDoc);
+	    	UpdateResult updateResult =
+	    			employeesDBCollection.replaceOne(eq("_id", new ObjectId(id)), newEmployeeDoc);
+	    	if(updateResult.getMatchedCount() < 1) {
+	    		throw new WebApplicationException(Response.Status.NOT_FOUND);
+	    	}
+	    	if(updateResult.getModifiedCount() < 1) {
+	    		throw new WebApplicationException(Response.Status.NOT_MODIFIED);
+	    	}
     	}
     }
     
